d32b00378e6a1d3e4157cd68b101893397185d25
Hunter Horsman <hhorsman@square-9.com>
2020-10-22 13:02:33 -0400

    Add routing

--- src/motowidget-server.cpp
+++ src/motowidget-server.cpp
@@ -1,15 +1,21 @@
+#include <Arduino.h>
+
+#include <Adafruit_MCP23017.h>
+#include <WiFi.h>
+#include <generic_i2c_rw.h>
+
 #include "Constants.h"
-#include "SwitchStates.h"
+
 #include "aWOT.h"
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 #include "freertos/FreeRTOS.h"
-#include <Adafruit_MCP23017.h>
-#include <Arduino.h>
-#include <WiFi.h>
-#include <generic_i2c_rw.h>
 
 #include "StaticFiles.h"
+#include "SwitchStates.h"
+
+#define btoa(x) ((x) ? "true" : "false")
+#define btoo(x) ((x) ? "on" : "off")
 
 WiFiServer server(80);
 Application app;
@@ -40,16 +46,49 @@ void blinkers() {
 // Route handlers
 void readStates(Request &req, Response &res) {
   res.printf(
-      "{\"turnL\":%d},\"turnR\":$d,\"brake\":$d,\"highbeam\":$d,\"neutral\":$d",
-      powerStates.turnL, powerStates.turnR, powerStates.brake,
-      powerStates.highbeam, powerStates.neutral);
+      "{\"turnL\":%s},\"turnR\":%s,\"brake\":%s,\"highbeam\":%s,\"neutral\":%s",
+      btoa(powerStates.turnL), btoa(powerStates.turnR), btoa(powerStates.brake),
+      btoa(powerStates.highbeam), btoa(powerStates.neutral));
 }
 
-void updateLed(Request &req, Response &res) {
-  // ledOn = (req.read() != '0');
-  // Serial.println(ledOn);
-  // digitalWrite(LED_BUILTIN, ledOn);
-  return readLed(req, res);
+void updateTurnL(Request &req, Response &res) {
+  bool state = (req.read() != '0');
+  Serial.printf("[API] Turn L: \%s", btoo(state));
+  mcp.digitalWrite(TURN_L, state);
+  powerStates.turnL = state;
+  return readStates(req, res);
+}
+
+void updateTurnR(Request &req, Response &res) {
+  bool state = (req.read() != '0');
+  Serial.printf("[API] Turn R: \%s", btoo(state));
+  mcp.digitalWrite(TURN_R, state);
+  powerStates.turnR = state;
+  return readStates(req, res);
+}
+
+void updateBrake(Request &req, Response &res) {
+  bool state = (req.read() != '0');
+  Serial.printf("[API] Brake: \%s", btoo(state));
+  mcp.digitalWrite(BRAKE_LIGHT, state);
+  powerStates.brake = state;
+  return readStates(req, res);
+}
+
+void updateHighbeam(Request &req, Response &res) {
+  bool state = (req.read() != '0');
+  Serial.printf("[API] Highbeam: \%s", btoo(state));
+  mcp.digitalWrite(HEADLIGHT_HIGH, state);
+  powerStates.highbeam = state;
+  return readStates(req, res);
+}
+
+void updateNeutral(Request &req, Response &res) {
+  bool state = (req.read() != '0');
+  Serial.printf("[API] Neutral: \%s", btoo(state));
+  mcp.digitalWrite(NEUTRAL, state);
+  powerStates.neutral = state;
+  return readStates(req, res);
 }
 
 void reset_mcp23017() {
@@ -98,25 +137,32 @@ void setup() {
   mcp.pinMode(SW_NEUTRAL, INPUT);
   mcp.pullUp(SW_NEUTRAL, HIGH);
 
-  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println(WiFi.localIP());
-
-  // app.get("/led", &readLed);
-  // app.put("/led", &updateLed);
-  // app.use(staticFiles());
-
-  // server.begin();
-
-  // mcp.readGPIOAB();
-
-  Serial.begin(115200);
-
+  // Set initial output states
   updateOutputsToCurrentInputStates();
+
+  // Connect to WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.printf("Connected to '%s', IP:", WIFI_SSID);
+  Serial.println(WiFi.localIP());
+
+  // Configure routes
+  app.get("/states", &readStates);
+  app.put("/turnL", &updateTurnL);
+  app.put("/turnR", &updateTurnR);
+  app.put("/brake", &updateBrake);
+  app.put("/highbeam", &updateHighbeam);
+  app.put("/neutral", &updateNeutral);
+  app.use(staticFiles());
+
+  // Start webserver
+  server.begin();
+
+  // Enable serial logging
+  Serial.begin(115200);
 
   // Set initial timer for blinking flashers
   lastBlink = millis();
@@ -124,12 +170,12 @@ void setup() {
 
 // Main execution
 void loop() {
-  // WiFiClient client = server.available();
+  // Handle HTTP requests
+  WiFiClient client = server.available();
 
-  // if (client.connected())
-  // {
-  //   app.process(&client);
-  // }
+  if (client.connected()) {
+    app.process(&client);
+  }
 
   // Scan I/O for changes to buttons and inputs
   SwitchStates::checkForButtonStateChanges(&mcp, &lastButtonStates,
