0b54d42a0842493d63ad463e55c7df69d9214360
Hunter Horsman <hhorsman@square-9.com>
2020-10-28 12:58:47 -0400

    Remove unused OTA code

--- src/motowidget-server.cpp
+++ src/motowidget-server.cpp
@@ -2,10 +2,6 @@
 
 #include <Adafruit_MCP23017.h>
 
-#ifdef OTA
-#include <ArduinoOTA.h>
-#endif
-
 #include <WiFi.h>
 #include <generic_i2c_rw.h>
 
@@ -158,45 +154,6 @@ void setup() {
   Serial.printf("\nConnected to '%s', IP: ", WIFI_SSID);
   Serial.println(WiFi.localIP());
 
-#ifdef OTA
-
-  ArduinoOTA.setHostname("MotoWidget");
-  ArduinoOTA.setPasswordHash("f9d6f5b1dd30990866441e85606794b3"); // flashm3
-
-  ArduinoOTA
-      .onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-          type = "sketch";
-        else // U_SPIFFS
-          type = "filesystem";
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
-        // using SPIFFS.end()
-        Serial.println("Start updating " + type);
-      })
-      .onEnd([]() { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-      })
-      .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-          Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-          Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-          Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-          Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-          Serial.println("End Failed");
-      });
-
-  ArduinoOTA.begin();
-
-#endif
-
   // Configure routes
   app.get("/states", &readStates);
   app.put("/turnL", &updateTurnL);
@@ -227,11 +184,6 @@ void loop() {
 
   // Update flashing lights
   blinkers();
-
-#if OTA
-  // OTA
-  ArduinoOTA.handle();
-#endif
 
   // Slow your roll
   delay(50);
